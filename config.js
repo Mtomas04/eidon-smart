282e9466d5e7a3ceba604d93f97e33e4d2864e3f
Miguel Tomas Barreto Ramirez <mbarretoramirez@udesa.edu.ar>
2026-08-24 22:53:48 -0300

    Update contact email and site URL in config

--- config.js
+++ config.js
@@ -1,13 +1,10 @@
 // Config central del sitio. Un solo lugar para tocar cuando cambien estos datos.
 export const CONFIG = {
-  // TODO: cambiar a contacto@eidonsmart.com cuando esté el dominio propio activo
-  contactEmail: "equipo.eidon.smart@gmail.com",
+    contactEmail: "contacto@eidonsmart.com",
+    siteUrl: "https://eidonsmart.com",
 
-  // TODO: cambiar a https://eidonsmart.com cuando el dominio esté apuntando acá
-  siteUrl: "https://eidon-smart.netlify.app",
-
-  social: {
-    linkedin: "https://www.linkedin.com/company/111421948",
-    instagram: "https://www.instagram.com/eidon.smart",
-  },
+    social: {
+          linkedin: "https://www.linkedin.com/company/111421948",
+          instagram: "https://www.instagram.com/eidon.smart",
+    },
 };
