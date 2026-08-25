b762ac063a1e38b7bf3acb634666001cd1f9298e
Miguel Tomas Barreto Ramirez <mbarretoramirez@udesa.edu.ar>
2026-08-24 23:19:24 -0300

    Update social media links in config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 // Config central del sitio. Un solo lugar para tocar cuando cambien estos datos.
 export const CONFIG = {
-    contactEmail: "contacto@eidonsmart.com",
-    siteUrl: "https://eidonsmart.com",
+      contactEmail: "contacto@eidonsmart.com",
+      siteUrl: "https://eidonsmart.com",
 
-    social: {
-          linkedin: "https://www.linkedin.com/company/111421948",
-          instagram: "https://www.instagram.com/eidon.smart",
-    },
+      social: {
+              linkedin: "https://www.linkedin.com/company/111421948",
+              instagram: "https://www.instagram.com/eidon_smart/",
+      },
 };
